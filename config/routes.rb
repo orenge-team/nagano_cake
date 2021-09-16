b36c323cb401a1965e63f5e4a9036a0295f99dbf
EC2 Default User <ec2-user@ip-172-31-10-0.ec2.internal>
2021-09-16 12:28:43 +0000

    [Add ]

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
-  
+
   devise_for :admins, controllers:{
     sessions:     'admins/sessions',
     passwords:    'admins/passwords',
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :public do
+  scope module: :public do
    resources :orders, only: [:new, :create, :index, :show]
    post 'orders/confirm' => 'orders#confirm'
    get 'orders/thanks' => 'orders#thanks'
