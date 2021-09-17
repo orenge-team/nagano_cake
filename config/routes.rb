2e210c936f28d79846dfe9c8ef1c745d9746acfb
Kazuki Hashimoto <abkeybg111177@gmail.com>
2021-09-17 09:28:10 +0000

    [add]publick-customers-unsubscribe

--- config/routes.rb
+++ config/routes.rb
@@ -32,9 +32,11 @@ Rails.application.routes.draw do
   end
 
    scope module: :public do
+
+    get "customers/unsubscribe" => "customers#unsubscribe"
+    patch "customers/withdraw" => "customers#withdraw"
     resource :customers, only: [:show, :edit, :update]
-    get "customers/unsubscribe" => "customer#unsubscribe"
-    patch "customers/withdraw" => "customer#withdraw"
    end
+
 
 end
