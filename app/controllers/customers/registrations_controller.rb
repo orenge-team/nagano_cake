a1069a46c9e8c3812a68e699cbf53d0aa31fe7f5
Kazuki Hashimoto <abkeybg111177@gmail.com>
2021-09-17 14:37:47 +0000

    devise

--- app/controllers/customers/registrations_controller.rb
+++ app/controllers/customers/registrations_controller.rb
@@ -3,13 +3,17 @@
 class Customers::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+   def after_sign_in_path_for(resource)
+    customers_path
+   end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name,:last_name,:first_name_kana,:last_name_kana,:email,:postal_code,:address,:phone_number,:encrypted_password])
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  end
   # GET /resource/sign_up
   # def new
   #   super
