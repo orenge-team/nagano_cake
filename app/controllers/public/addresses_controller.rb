d97797dbb038b18273d52c843ca14d46ee6c5152
EC2 Default User <ec2-user@ip-172-31-10-0.ec2.internal>
2021-09-19 06:34:53 +0000

    Add

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,4 +1,5 @@
 class Public::AddressesController < ApplicationController
+
   def index
     @address = Address.new
     @addresses = current_customer.addresses
@@ -8,9 +9,9 @@ class Public::AddressesController < ApplicationController
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     if @address.save
-      redirect_to address_path(current_customer)
+      redirect_to addresses_path(current_customer)
     else
-      @address = current_customer.public_addresses
+      @address = current_customer.addresses
       render :index
     end
   end
@@ -22,21 +23,21 @@ class Public::AddressesController < ApplicationController
   def update
     @address = Address.find(params[:id])
     if @address.update(address_params)
-    redirect_to addresses_path(@address)
+    redirect_to addresses_path
     else
     render :edit
     end
   end
 
   def destroy
-    @address = Address.find(params[:id])
-    @address.destroy
-    redirect_to addresses_path
-
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path(current_customer)
   end
 
   private
   def address_params
     params.require(:address).permit(:name, :postal_code, :address, :customer_id)
   end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,5 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :addresses
+  has_many :addresses, dependent: :destroy
 end
