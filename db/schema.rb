381460dc66a2b1ebfec0304e08934213bf0d771a
Kazuki Hashimoto <abkeybg111177@gmail.com>
2021-09-19 13:27:59 +0000

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_19_023943) do
+ActiveRecord::Schema.define(version: 2021_09_19_132607) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_09_19_023943) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "customer_id"
   end
 
   create_table "admins", force: :cascade do |t|
