85d929986ce80f9ac2a932e888f891dbd9d0b867
EC2 Default User <ec2-user@ip-172-31-31-132.ec2.internal>
2021-09-19 08:16:39 +0000

    [Add]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,10 +1,7 @@
 class Admin::ItemsController < ApplicationController
 
   def index
-    @items = Item.all
-    # @item = 
-    # @genre = @item.genre.name
-
+    @items = Item.page(params[:page])
   end
 
   def new
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
- 
+
   belongs_to :genre
   attachment :image
 
-  
+  enum is_active: { "販売休止中": false, "販売中": true }
+
 end
