0e0ff695725ab5098aaa5ed9c123464ac7b2b328
Kazuki Hashimoto <abkeybg111177@gmail.com>
2021-09-20 03:46:31 +0000

    add

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,15 @@
 class  Public::ItemsController < ApplicationController
   def index
-    @item = Item.all
+    @items = Item.page(params[:page])
   end
 
   def show
     @item = Item.find(params[:id])
+    @genre = @item.genre.name
   end
 end
+
+private
 
 def item_params
 
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,6 +11,8 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    @order = Order.find(params[:id])
+    
     
   end
 
