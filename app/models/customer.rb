7164810785732bccefd5a27e7652247f586dafcb
EC2 Default User <ec2-user@ip-172-31-31-132.ec2.internal>
2021-09-20 08:05:05 +0000

    詳細ページ追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,7 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
- enum is_deleted: { "有効": false , "退会済み": true }
+  enum is_deleted: { "有効": false , "退会済み": true }
 
   has_many :orders, dependent: :destroy
 
