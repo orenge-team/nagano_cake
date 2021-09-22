6cbaff04d010a70e16e838d8c8a8efcc4dc0be5b
EC2 Default User <ec2-user@ip-172-31-10-0.ec2.internal>
2021-09-22 02:43:10 +0000

    add

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  has_many :Order_items , dependent: :destroy
+  has_many :order_items , dependent: :destroy
 
   enum payment_method:{"クレジットカード": 0, "銀行振込": 1}
 end
