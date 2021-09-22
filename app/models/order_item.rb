2394e978336118f0f982f66eed07b6cca6f7ecae
EC2 Default User <ec2-user@ip-172-31-31-132.ec2.internal>
2021-09-22 13:44:07 +0000

    showページ途中

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,6 +3,14 @@ class OrderItem < ApplicationRecord
   belongs_to :item
   belongs_to :order
   enum making_status:{"着手不可":0, "制作待ち":1,"製作中":2,"制作完了":3}
+  
+  def sum_price
+    item.add_tax_price * count
+  end
+  
+  def add_tax_price
+    (self.price * 1.10).round
+  end
 
 
 end
