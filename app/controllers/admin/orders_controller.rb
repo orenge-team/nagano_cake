93135c819f0707b5f9f2639441b5d903fe3311cc
Kazuki Hashimoto <abkeybg111177@gmail.com>
2021-09-22 07:36:04 +0000

    order

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,13 +1,19 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def update
     @order = Order.find(params[:id])
-    @status = params[:order][:status].to_i
-    @order.update(status: status)
-    redirect_to admin_order_path(order)
+    @order.update(status: @order.status)
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+
+  def order_params
+     params.require(:order).permit(:status)
   end
 
 
