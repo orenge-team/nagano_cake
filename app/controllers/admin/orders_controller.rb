4561b196d130e2b9503976e36591662707d085cf
EC2 Default User <ec2-user@ip-172-31-31-132.ec2.internal>
2021-09-22 07:47:51 +0000

    show画面途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,14 +1,19 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
 
   def update
     @order = Order.find(params[:id])
-    @status = params[:order][:status].to_i
-    @order.update(status: status)
-    redirect_to admin_order_path(order)
+    @order.update(order_params)
+    redirect_to admin_order_path(@order)
   end
+
+ def order_params
+    params.require(:order).permit(:status)
+ end
 
 
 end
