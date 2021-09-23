b8a42732ac03cbd96e04150121af17f9205e273b
EC2 Default User <ec2-user@ip-172-31-31-132.ec2.internal>
2021-09-23 08:47:27 +0000

    showページ機能完成

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,15 +1,16 @@
 class Admin::OrderItemsController < ApplicationController
-    
-   def update
-     @order_items = Order_items.find(params[:id])
-     @order_items.update(order_item_params)
-     redirect_to admin_order_path(order)
-   end 
 
-    
+   def update
+     @order = Order.find(params[:id])
+     @order_items = OrderItem.find(params[:id])
+     @order_items.update(order_item_params)
+     redirect_to admin_order_path(@order)
+   end
+
+
  private
    def order_item_params
      params.require(:order_item).permit(:making_status)
    end
- 
+
 end
