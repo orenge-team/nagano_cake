60fcacad0c8f61e9597d19b0403f34be52f7e5e2
Kazuki Hashimoto <abkeybg111177@gmail.com>
2021-09-25 03:55:28 +0000

    search

---
+++ app/controllers/public/searches_controller.rb
@@ -0,0 +1,11 @@
+class Public::SearchesController < ApplicationController
+   before_action :authenticate_customer!
+   def search
+    @range = params[:range]
+
+    if @range == "Item"
+      @items = Item.looks(params[:search], params[:word])
+
+    end
+   end
+end
---
+++ test/controllers/puclic/searches_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Puclic::SearchesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
