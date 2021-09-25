3a285b1cb0a26d39c87df28933452fedc73a5877
Lispooh13 <Riaya1203@gmail.com>
2021-09-25 18:42:47 +0000

    [レイアウト]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,6 @@
 class  Public::ItemsController < ApplicationController
   def index
-    @items = Item.page(params[:page]).per(6).order(:id)
+    @items = Item.page(params[:page]).per(8).order(:id)
     @customer = current_customer
   end
 
