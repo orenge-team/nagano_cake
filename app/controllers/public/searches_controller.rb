034cdac55b27cf0d42e316dd283f23d428e2a460
Lispooh13 <Riaya1203@gmail.com>
2021-09-26 04:18:39 +0000

    [Add]検索

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,12 @@
 class Public::SearchesController < ApplicationController
    def search
     @range = params[:range]
+    @items = Item.page(params[:page]).per(8).order(:id)
+    @genres = Genre.all
+     if params[:genre_id].present?
+       @genre = Genre.find(params[:genre_id])
+       @items = @genre.items.page(params[:page]).per(8).order(:id)
+     end
 
     if @range == "Item"
       @items = Item.looks(params[:search], params[:word])
